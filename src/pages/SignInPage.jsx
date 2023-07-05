c8fa93ed046f8a9e17093a32460e423b69e1a102
CaioNeme <caiocesarneme@gmail.com>
2023-07-05 12:32:34 -0300

    sign-in / finished

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,21 +1,37 @@
 import styled from "styled-components"
 import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import axios from "axios"
 
 
 export default function SignInPage() {
   const navigate = useNavigate()
+  const [login, setLogin] = useState({
+    email: "",
+    password: ""
+  });
+
+  function handleChange(event) {
+    const newLogin = { ...login };
+    newLogin[event.target.name] = event.target.value;
+    setLogin(newLogin);
+  }
 
   return (
     <SingInContainer>
       <form onSubmit={event => {
         event.preventDefault();
-        // axios e se der certo leva pra /home
-        navigate("/home")
+        axios.post("http://localhost:5000/", login).then(() => navigate("/home")).catch((error) => {
+          const erro = (error.response.status);
+          if (erro === 422) return alert("Confira os dados!");
+          if (erro === 404) return alert("Email não encontrado");
+          if (erro === 401) return alert("Senha incorreta");
+        })
       }}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
+        <input required onChange={handleChange} value={login.email} name="email" placeholder="E-mail" type="email" />
+        <input required onChange={handleChange} value={login.password} name="password" placeholder="Senha" type="password" autocomplete="new-password" />
         <button>Entrar</button>
       </form>
 
